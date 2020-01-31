bfc650628556bc8148e131121056811463587f5a
Alisha Pratik Patel <alishapatel79@gmail.com>
2020-01-31 12:15:09 -0500

    commit 4

--- Week3/Models/GetInTouch.cs
+++ Week3/Models/GetInTouch.cs
@@ -7,5 +7,11 @@ namespace Week3.Models
 {
     public class GetInTouch
     {
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public string Email { get; set; }
+        public string Contact { get; set; }
+        public string Message { get; set; }
+        public DateTime Datecreated { get; set; }
     }
 }
